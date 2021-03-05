a80cd697a4c30f827696bac546064c4257fd3356
Sujan Ahmed <sujanmec804@gmail.com>
2021-03-05 11:15:41 +0600

    Second day mid commit

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,8 @@
+from leads.models import Agent, Lead, User
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Agent)
+admin.site.register(Lead)
+admin.site.register(User)
+
